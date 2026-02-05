e30cc6f30b2ec6948c33d82e7a12d922cd1df2b8
Salman <sseitmametov@gmail.com>
2026-02-05 15:19:22 +0600

    пока что актуальный код без исправления багов и проблем, можно на него откатиься

--- src/main/java/com/example/library/security/SecurityConfig.java
+++ src/main/java/com/example/library/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -33,9 +34,9 @@ public class    SecurityConfig {
                                 "/v3/api-docs",
                                 "/auth/**"
                         ).permitAll()
-                        .anyRequest().permitAll()
-
-                );
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
